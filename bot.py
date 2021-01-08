c0dcf4d88d8a4a910d57e564c78be930d1d2beb5
rphly <raphaelyeejunkai@gmail.com>
2021-01-08 11:37:22 +0800

    inline

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from uuid import uuid4
+from telegram import InlineQueryResultArticle, ParseMode, InputTextMessageContent, Update
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler, CallbackContext, MessageHandler, Filters
+from telegram.utils.helpers import escape_markdown
 from consts import Constants
 import logging
 import os
@@ -13,11 +16,39 @@ dispatcher = updater.dispatcher
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=Constants.INTRODUCTION)
 
+def inlinequery(update: Update, context: CallbackContext) -> None:
+    """Handle the inline query."""
+    query = update.inline_query.query
+    results = [
+        InlineQueryResultArticle(
+            id=uuid4(), title="Caps", input_message_content=InputTextMessageContent(query.upper())
+        ),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Bold",
+            input_message_content=InputTextMessageContent(
+                f"*{escape_markdown(query)}*", parse_mode=ParseMode.MARKDOWN
+            ),
+        ),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Italic",
+            input_message_content=InputTextMessageContent(
+                f"_{escape_markdown(query)}_", parse_mode=ParseMode.MARKDOWN
+            ),
+        ),
+    ]
+
+    update.inline_query.answer(results)
+
+
 # define handlers
 start_handler = CommandHandler('start', start)
+inline_handler = InlineQueryHandler(inlinequery)
 
 # add handlers
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(inline_handler)
 
 # start webhook
 updater.start_webhook(listen="0.0.0.0",
